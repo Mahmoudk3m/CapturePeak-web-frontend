876b7a15fb411e3e678cdfe5ff5d39c5b6d5589c
Mahmoudk3m <mahmoudk3m.mk@gmail.com>
2024-05-04 21:12:26 +0300

    feat: Add password visiblity toggle

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -4,6 +4,9 @@ import loginSchema from "../schemas/login";
 import ErrorMessage from "@/Shared/components/ErrorMessage";
 import { useNavigate } from "react-router-dom";
 import { useUserLogin } from "../api/userLogin";
+import EyeOn from "@/assets/icons/EyeOn";
+import EyeOff from "@/assets/icons/EyeOff";
+import { useState } from "react";
 
 export default function LoginForm() {
   const navigate = useNavigate();
@@ -14,10 +17,15 @@ export default function LoginForm() {
     formState: { errors }
   } = useForm({ resolver: yupResolver(loginSchema) });
 
-  const { mutate } = useUserLogin();
+  const { mutate, isError } = useUserLogin();
 
   const onSubmit = (data: AuthTypes.Payload) => {
     mutate(data);
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePassword = () => {
+    setShowPassword((prev) => !prev);
   };
 
   return (
@@ -41,7 +49,7 @@ export default function LoginForm() {
       <div className="relative">
         <input
           id="password"
-          type="password"
+          type={showPassword ? "text" : "password"}
           className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
           placeholder="Password"
           {...register("password")}
@@ -49,7 +57,18 @@ export default function LoginForm() {
         <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
           Password
         </label>
+        {showPassword ? (
+          <div className="absolute right-0 top-2 cursor-pointer p-2 z-10" onClick={togglePassword}>
+            <EyeOn />
+          </div>
+        ) : (
+          <div className="absolute right-0 top-2 cursor-pointer p-2 z-10" onClick={togglePassword}>
+            <EyeOff />
+          </div>
+        )}
+
         {errors.password?.message && <ErrorMessage message={errors.password.message} />}
+        {isError && <ErrorMessage message="Wrong username or password" />}
       </div>
       <div className="relative">
         <button className="bg-primary-500 text-white rounded-md px-2 py-1">Submit</button>
--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -4,6 +4,9 @@ import { useForm } from "react-hook-form";
 import ErrorMessage from "@/Shared/components/ErrorMessage";
 import { useNavigate } from "react-router-dom";
 import { useUserRegister } from "../api/userRegister";
+import { useState } from "react";
+import EyeOn from "@/assets/icons/EyeOn";
+import EyeOff from "@/assets/icons/EyeOff";
 
 export default function RegisterForm() {
   const navigate = useNavigate();
@@ -13,10 +16,15 @@ export default function RegisterForm() {
     formState: { errors }
   } = useForm({ resolver: yupResolver(registerSchema) });
 
-  const { mutate } = useUserRegister();
+  const { mutate, isError } = useUserRegister();
 
   const onSubmit = (data: AuthTypes.Payload) => {
     mutate(data);
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePassword = () => {
+    setShowPassword((prev) => !prev);
   };
 
   return (
@@ -40,7 +48,7 @@ export default function RegisterForm() {
       <div className="relative">
         <input
           id="password"
-          type="password"
+          type={showPassword ? "text" : "password"}
           className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
           placeholder="Password"
           {...register("password")}
@@ -48,12 +56,21 @@ export default function RegisterForm() {
         <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
           Password
         </label>
+        {showPassword ? (
+          <div className="absolute right-0 top-2 cursor-pointer p-2 z-10" onClick={togglePassword}>
+            <EyeOn />
+          </div>
+        ) : (
+          <div className="absolute right-0 top-2 cursor-pointer p-2 z-10" onClick={togglePassword}>
+            <EyeOff />
+          </div>
+        )}
         {errors.password?.message && <ErrorMessage message={errors.password.message} />}
       </div>
       <div className="relative">
         <input
           id="confirmPassword"
-          type="password"
+          type={showPassword ? "text" : "password"}
           className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
           placeholder="Confirm  Password"
           {...register("confirmPassword")}
@@ -61,7 +78,17 @@ export default function RegisterForm() {
         <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
           Confirm Password
         </label>
+        {showPassword ? (
+          <div className="absolute right-0 top-2 cursor-pointer p-2 z-10" onClick={togglePassword}>
+            <EyeOn />
+          </div>
+        ) : (
+          <div className="absolute right-0 top-2 cursor-pointer p-2 z-10" onClick={togglePassword}>
+            <EyeOff />
+          </div>
+        )}
         {errors.confirmPassword?.message && <ErrorMessage message={errors.confirmPassword.message} />}
+        {isError && <ErrorMessage message="User already exists" />}
       </div>
       <div className="relative">
         <button className="bg-primary-500 text-white rounded-md px-2 py-1">Submit</button>
