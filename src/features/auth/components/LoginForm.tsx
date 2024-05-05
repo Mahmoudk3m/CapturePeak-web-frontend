b3106fd919bd47af8fed235c0318ea978294c08d
Mahmoudk3m <mahmoudk3m.mk@gmail.com>
2024-05-05 17:34:25 +0300

    fix: bug when you click on label it selects it instead of opening input field

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -41,7 +41,10 @@ export default function LoginForm() {
           placeholder="Username"
           {...register("username")}
         />
-        <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
+        <label
+          htmlFor="username"
+          className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+        >
           Username
         </label>
         {errors.username?.message && <ErrorMessage message={errors.username.message} />}
@@ -54,7 +57,10 @@ export default function LoginForm() {
           placeholder="Password"
           {...register("password")}
         />
-        <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
+        <label
+          htmlFor="password"
+          className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+        >
           Password
         </label>
         {showPassword ? (
--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -40,7 +40,10 @@ export default function RegisterForm() {
           placeholder="Username"
           {...register("username")}
         />
-        <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
+        <label
+          htmlFor="username"
+          className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+        >
           Username
         </label>
         {errors.username?.message && <ErrorMessage message={errors.username.message} />}
@@ -53,7 +56,10 @@ export default function RegisterForm() {
           placeholder="Password"
           {...register("password")}
         />
-        <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
+        <label
+          htmlFor="password"
+          className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+        >
           Password
         </label>
         {showPassword ? (
@@ -75,7 +81,10 @@ export default function RegisterForm() {
           placeholder="Confirm  Password"
           {...register("confirmPassword")}
         />
-        <label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">
+        <label
+          htmlFor="confirmPassword"
+          className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+        >
           Confirm Password
         </label>
         {showPassword ? (
